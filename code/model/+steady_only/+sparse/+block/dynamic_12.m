db6794c52ec7c48dac2e78b5c0d14d113047c234
Manuel González <pgonzale@espol.edu.ec>
2026-05-29 09:26:47 -0400

    Delete code directory to leave only replication folder

--- code/model/+steady_only/+sparse/+block/dynamic_12.m
+++
@@ -1,6 +0,0 @@
-function [y, T] = dynamic_12(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(118)=y(151)*(1-params(19))/(1-params(13));
-  y(119)=y(151)*params(19)/params(13);
-  y(117)=y(150)+y(36)*(1-params(20))/y(77);
-  y(125)=y(160)*y(152)*params(9);
-end
--- code/model/+steady_only/+sparse/+block/static_1.m
+++
@@ -1,9 +0,0 @@
-function [y, T] = static_1(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(68)=params(30);
-  y(69)=params(27);
-  y(70)=params(33);
-  y(65)=params(24);
-  y(66)=params(25);
-  y(67)=params(26);
-  y(71)=params(67);
-end
